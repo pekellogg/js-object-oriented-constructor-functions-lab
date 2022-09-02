684ccc8b064800900fe59251765778f066936ab7
Peyton Kellogg <pekellogg@hotmail.com>
2022-09-01 17:32:49 -0700

    commit tree de2a7ed3fe4c649877409fe91c26153386128c60

---
+++ index.js
@@ -0,0 +1,19 @@
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+  // this.sayHello = function() {
+  //     console.log(`Hello everybody, my name is ${this.name} whom you've been mailing at ${this.email}!`);
+  // };
+}
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
